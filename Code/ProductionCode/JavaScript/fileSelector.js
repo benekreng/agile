c7371af0ea88ae564f6f6c0d85a15b1e8b378f63
Lasana <54402040+Lasana1@users.noreply.github.com>
2022-08-04 21:06:31 +0100

    Commit

--- Code/ProductionCode/JavaScript/fileSelector.js
+++ Code/ProductionCode/JavaScript/fileSelector.js
@@ -22,6 +22,34 @@ input.addEventListener('change', function(e)
         console.log(loadedData)
         // createChartWithData(loadedData)
 
+        function parser(arr){
+            masterObj = {}
+            // pull the titles (should be the first index)
+            // this will perm remove titles from the array
+            titles = arr.shift()
+            for(let x = 0; x < titles.length; x++){
+                if(!(titles[x] in masterObj)){
+                    masterObj[titles[x]]= {}
+                    masterObj[titles[x]]["MasterIndex"] = x
+                    masterObj[titles[x]]["Data"] = []
+                }
+                else{
+                    console.log("Error, duplicate column title at index,", x, " Duplicate column name: ", titles[x])
+                }
+            }
+            for(const key in masterObj){
+                masterIndex = masterObj[key].MasterIndex
+                for(let x = 0; x < arr.length; x++)
+                {
+                    masterObj[key]["Data"].push(arr[x][masterIndex])
+                }
+            }
+            console.log("titles of the loaded data = "+titles);
+            return [titles,masterObj];
+        }
+
+        parser(loadedData);
+
         // update the first part of the NavBar 
         document.getElementById("navBar1").style.backgroundColor = "red";
         document.getElementById("navBar2").style.backgroundColor = "inherit";
